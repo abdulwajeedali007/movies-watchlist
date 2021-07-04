e355c237b8bf7488c239c0f0b4601fd7bec37cbc
Wajeed <abdulwajeedali007@gmail.com>
2021-07-05 00:51:08 +0530

    final commit with images

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -1,24 +1,20 @@
-import React from 'react'
-import Movie from './Movie'
-
+import React from "react";
+import Movie from "./Movie";
 
 export default function Item(props) {
-    const results = props.data;
-    
+  const results = props.data;
 
-    
-    return (
-        <div className="item__main">
-            {results.length > 0 
-               ?
-                    <div>
-                    {results.map((movie) => (
-                       <Movie movie={movie} key={movie.id}/>
-                    ))}
-                </div>
-                :  
-                <div className="data__loading">Type movie name in search....</div> 
-            }
+  return (
+    <div className="item__main">
+      {results.length > 0 ? (
+        <div>
+          {results.map((movie) => (
+            <Movie movie={movie} key={movie.id} />
+          ))}
         </div>
-    )
+      ) : (
+        <div className="data__loading">Type movie name in search....</div>
+      )}
+    </div>
+  );
 }
